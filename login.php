186642abcadae419d85a856d0a2fbbb53e4739bb
SanderBK <sanderbushkrogh@mail.com>
2018-11-08 11:51:52 +0100

    Updated homepage with basic layout

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Logging in, please wait</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="stylesheet/styles.css" />
+    </head>
+    <?php
+        $servername="localhost";
+        $username="root";
+        $password="";
+        $dbname="neurawind";
+
+        $mail = $_POST["mail"];
+        $userPassword = $_POST["password"];
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if($conn->connect_error){
+            die("død");
+        }
+
+        $sql = "SELECT password FROM users WHERE email='$mail'";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0){
+            $information = $result->fetch_assoc();
+            if($userPassword == $information['password']){
+                header("Location:home.html");
+            }else{
+                header("Location:index.html");
+            }
+        }else{
+            header("Location:index.html");
+        }
+
+        
+        $conn->close();
+    ?>
+</html>
