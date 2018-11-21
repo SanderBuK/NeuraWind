48a575fbbc27cde2eeedfa17ec362bcd80da93a0
GenjiOriginal <37450278+GenjiOriginal@users.noreply.github.com>
2018-11-21 12:02:58 +0100

    Add files via upload
    Added new design and sorted scripts by folders

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 # NeuraWind
-
-#1 Setup
-Create a database called 'neuralink'
-In the database add a table called 'users' with 5 columns:
-|
- \ Column1: Name is 'user_id', it is of type int, and enable AUTO_INCREMENT.
- | Column2: Name is 'email', it is of type text.
- | Column3: Name is 'password', it is of type text.
- | Column4: Name is 'username', it is of type text.
- | Column5: Name is 'hasdb', it is of type BOOLEAN, with a default value of 0.
-
-#2 Download the files
-Download the files into a localhost server like XAMPP, under htdocs.
-Start the server and naivgate to the folder in localhost.
---
+++ php/login.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('connect_db.php');
+
+    $mail = $_POST["mail"];
+    $userPassword = $_POST["password"];
+
+    //Used to retrieve information about the user on the home.php page.
+    session_start();
+    $_SESSION['usermail'] = $mail;
+    
+    $sql = "SELECT * FROM users WHERE email='$mail'";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        $information = $result->fetch_assoc();
+        if($userPassword == $information['password']){
+            header("Location:home.php");
+        }else{
+            header("Location:../index.html");
+        }
+    }else{
+        header("Location:../index.html");
+    }
+
+    $conn->close();
+?>
