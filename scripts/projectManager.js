2fc7ccab7d3d5d673a729b312d9b5655920b89b4
SanderBuK <31419285+SanderBuK@users.noreply.github.com>
2018-11-24 18:02:10 +0100

    Add files via upload

--- scripts/projectManager.js
+++ scripts/projectManager.js
@@ -1,20 +1,20 @@
 function addProject() {
-    location.href = ('add_project.php');
+    location.href = ('php/add_project.php');
 }
 
-function changeProject(project_id){
+function changeProject(project_id) {
     sessionStorage.project_id = project_id;
-    location.href = ('load_project.php?project_id=' + project_id);
+    location.href = ('php/load_project.php?project_id=' + project_id);
 }
 
-function saveProject(count){
+function saveProject(count) {
     project_id = sessionStorage.getItem("project_id");
     project_title = encodeURIComponent(document.getElementById("project_title").value);
-    stringPath = 'save_project.php?title=' + project_title + "&project_id=" + project_id + "&count=" + count;
+    stringPath = 'php/save_project.php?title=' + project_title + "&project_id=" + project_id + "&count=" + count;
 
     titles = [];
     contents = [];
-    for(i = 0; i < count; i++){
+    for (i = 0; i < count; i++) {
         stringPath = stringPath.concat("&title" + i + "=" + encodeURIComponent(document.getElementById("title" + i).value));
         stringPath = stringPath.concat("&content" + i + "=" + encodeURIComponent(document.getElementById("content" + i).value));
     }
@@ -22,16 +22,53 @@ function saveProject(count){
     location.href = (stringPath);
 }
 
-function addContent(type){
+function addContent(type) {
     project_id = sessionStorage.getItem("project_id");
-    location.href = ("add_content.php?project_id=" + project_id + "&type=" + type);
+    location.href = ("php/add_content.php?project_id=" + project_id + "&type=" + type);
 }
 
-function removeContents(contentID){
+function removeContents(contentID) {
+    console.log(contentID);
     project_id = sessionStorage.getItem("project_id");
-    location.href = ("remove_content.php?project_id=" + project_id + "&contentID=" + contentID);
+    location.href = ("php/remove_content.php?project_id=" + project_id + "&contentID=" + contentID);
 }
 
-function removeProject(project_id){
-    location.href = ("remove_project.php?project_id=" + project_id);
+function removeProject(project_id) {
+    location.href = ("php/remove_project.php?project_id=" + project_id);
+}
+
+function shareProjectModal() {
+    var modal = document.getElementById('shareModal');
+
+    var btn = document.getElementById("shareBtn");
+
+    var closeBtn = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function searchShare() {
+    console.log("searchShare");
+    project_id = sessionStorage.getItem("project_id");
+    search = document.getElementById("search").value;
+    location.href = ("php/search_share.php?project_id=" + project_id + "&search=" + search);
+}
+
+function shareProject(searchUser_id) {
+    console.log("shareProject");
+    project_id = sessionStorage.getItem("project_id");
+    location.href = ("php/share_project.php?project_id=" + project_id + "&searchUser_id=" + searchUser_id);
 }
