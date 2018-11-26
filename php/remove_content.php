75c4022290afc00983ebaa9bf7e09221f78e78d1
SanderBuK <31419285+SanderBuK@users.noreply.github.com>
2018-11-26 22:59:38 +0100

    Project and content bug-fixes
    - Fixed a problem where when the creator of a project deleted it, the users he had shared it with got an error when trying to access it.
    - You are now only able to share a project once (Except it the receiver has deleted it)
    - Fixed an error where you weren't able to delete text boxes..

--- php/remove_content.php
+++ php/remove_content.php
@@ -1,14 +1,24 @@
 <?php
     require_once("with_user_project_id.php");
     
-    $content_id = $_GET['contentID'];
+    $content_row = $_GET['contentID'];
 
     $sqlProjectpath = "SELECT projectpath FROM user" . $user_id . "table WHERE project_id=" . $project_id;
     $resultProjectpath = $conn->query($sqlProjectpath);
     $projectpath = $resultProjectpath->fetch_assoc()['projectpath'];
 
-    $sqlRemoveContent = "DELETE FROM " . $projectpath . " WHERE content_id = " . $content_id;
+    $sqlGetContent = "SELECT * FROM " . $projectpath;
+    $resultContent = $conn->query($sqlGetContent);
 
+    //Get the real content_id from the content on the corresponding row.
+    for($i = 1; $i <= intval($content_row); $i++){
+        $row = $resultContent->fetch_assoc();
+        if($i == intval($content_row)){
+            $content_id = $row['content_id'];
+        }
+    }
+
+    $sqlRemoveContent = "DELETE FROM " . $projectpath . " WHERE content_id = " . $content_id;
     mysqli_query($conn, $sqlRemoveContent);
 
     header("Location:load_project.php?project_id=" . $project_id);
